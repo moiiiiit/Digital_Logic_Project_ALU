28bd1d41ac285e5a8e09098e975200fa182f6117
jacob charles richard <jacobcrichard8@gmail.com>
2020-04-27 22:53:56 -0500

    Register function

--- ALU.v
+++ ALU.v
@@ -1,3 +1,16 @@
+//=============================================
+// Register
+//=============================================
+module Register(clk,in,out);
+  parameter n=16;//width
+  input clk;
+  input [n-1:0] in;
+  output [n-1:0] out;
+  reg [n-1:0] out;
+  always @(posedge clk)
+  out = in;
+ endmodule
+
 
 module testbench();
 initial begin
